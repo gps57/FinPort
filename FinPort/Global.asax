e63b786c51a3b56e83edd75a50d1ea0356d48539
gps57 <glen@swiftbt.com>
2020-09-02 11:24:51 -0400

    Initial commit

---
+++ FinPort/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinPort.MvcApplication" Language="C#" %>
---
+++ FinPort/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinPort.Startup))]
+namespace FinPort
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
